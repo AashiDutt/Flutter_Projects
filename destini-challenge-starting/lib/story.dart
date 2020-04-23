bf2b7c9ee0fb1613e7c575e34cd1133fa2ff4bb9
AashiDutt <aashi.dutt3@gmail.com>
2020-04-23 16:54:54 +0530

    initial commit

---
+++ destini-challenge-starting/lib/story.dart
@@ -0,0 +1,15 @@
+
+class Story{
+  String storyTitle ; //storyTitle to store the story text
+  String choice1;  // stores text for choice 1
+  String choice2;  // stores text for choice 2
+
+  Story({
+    this.storyTitle,
+    this.choice1,
+    this.choice2
+  });
+
+}
+
+
